002ee7c0bcdba0b26f5eee5a182e8a961555bb60
bartomsilva <bartomsilva@gmail.com>
2023-08-03 14:16:11 -0300

    add userRouter no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from 'dotenv'
+import { userRouter } from "./routes/userRouter"
 
 dotenv.config()
 
@@ -12,5 +13,12 @@ const PORT = Number(process.env.PORT) || 3000
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
+//================ ROTAS
+
+server.use("/users", userRouter)
+
+
+
+
 
 
